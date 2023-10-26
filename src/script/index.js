b58826185da2a1eaa98edb759395e0222760327e
Vergos1 <legendsmobajl4@gmail.com>
2023-10-26 09:27:02 +0300

    layout: popular, trending sections

---
+++ src/script/index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const trendingSlider = new Swiper('.trending__slider', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    simulateTouch: window.innerWidth <= 741,
+    touchRatio: 1,
+  });
+});
+// swiper init
+
+
+let lastWidth = window.innerWidth;
+
+window.onresize = function() {
+  const breakpoints = [601, 741, 992];
+  const currentWidth = window.innerWidth;
+  for(let i = 0; i < breakpoints.length; i++) {
+    if ((lastWidth < breakpoints[i] && currentWidth >= breakpoints[i]) ||
+     (lastWidth >= breakpoints[i] && currentWidth < breakpoints[i])) {
+      lastWidth = currentWidth;
+      location.reload();
+      break;
+    }
+  }
+};
+// oversize auto reload
