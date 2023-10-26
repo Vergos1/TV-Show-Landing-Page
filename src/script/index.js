ecc34916ab492b1e7c64c4d2811af93cfbc23242
Vergos1 <legendsmobajl4@gmail.com>
2023-10-26 10:46:12 +0300

    layout: release section

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const trendingSlider = new Swiper('.trending__slider', {
-    slidesPerView: 2,
+    slidesPerView: window.innerWidth <= 600 ? 2 : 4,
     spaceBetween: 10,
     simulateTouch: window.innerWidth <= 741,
     touchRatio: 1,
@@ -8,13 +8,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 // swiper init
 
-
 let lastWidth = window.innerWidth;
 
-window.onresize = function() {
+window.onresize = function () {
   const breakpoints = [601, 741, 992];
   const currentWidth = window.innerWidth;
-  for(let i = 0; i < breakpoints.length; i++) {
+  for (let i = 0; i < breakpoints.length; i++) {
     if ((lastWidth < breakpoints[i] && currentWidth >= breakpoints[i]) ||
      (lastWidth >= breakpoints[i] && currentWidth < breakpoints[i])) {
       lastWidth = currentWidth;
